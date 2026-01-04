39294c643d5636e47e339e0ef7827cc18fae2ccd
shreyaww <2020.shreya.singh@ves.ac.in>
2026-01-04 17:00:14 +0530

    enabling swagger in development env

--- Middleware_Auth/Program.cs
+++ Middleware_Auth/Program.cs
@@ -36,7 +36,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Authentication-Shreya");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
